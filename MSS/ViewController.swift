4055d26af5ed634aaa152a5e6b91fce85a0bfe19
ilya Kurin <i.kurin@madbrains.ru>
2021-03-25 14:51:12 +0400

    Initial Commit

---
+++ MSS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MSS
+//
+//  Created by Ilya Kurin on 25/03/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
